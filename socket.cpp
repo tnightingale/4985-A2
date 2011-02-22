045e19159824e0dbed2de4ed45b9298e20afbd8c
thegreat <tom@tnightingale.com>
2011-02-22 13:07:13 -0800

    removed linger

--- socket.cpp
+++ socket.cpp
@@ -33,10 +33,10 @@ bool Socket::listen(PSOCKADDR_IN pSockAddr) {
     emit status(output);
     // TODO: Might want to call linger here.
     // allow the TCP packets to linger for up to 10 seconds
-    LINGER* linger = (LINGER*) malloc(sizeof(LINGER));
-    linger->l_onoff = 1;
-    linger->l_linger = 2;
-    setsockopt(socket_, SOL_SOCKET, SO_LINGER, (char*) linger, sizeof(linger));
+    //LINGER* linger = (LINGER*) malloc(sizeof(LINGER));
+    //linger->l_onoff = 1;
+    //linger->l_linger = 2;
+    //setsockopt(socket_, SOL_SOCKET, SO_LINGER, (char*) linger, sizeof(linger));
 
     return true;
 }
